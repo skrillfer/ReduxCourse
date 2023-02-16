1f6cbb7c0782019a487996db9acb15d470d37eb5
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-15 23:14:28 -0600

    subscribe to store and unsubscribe

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import { addTask, removeTask } from './action';
 import store from './store';
 
+const unsubscribe = store.subscribe(() =>
+  console.log('Updated', store.getState())
+);
+
 store.dispatch(addTask('Task1'));
+console.log(store.getState());
 store.dispatch(removeTask(1));
+console.log(store.getState());
+
+unsubscribe();
+store.dispatch(addTask('Task2'));
 console.log(store.getState());
