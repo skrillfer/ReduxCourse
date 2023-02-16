531620d98655ad5f030c5f2328a6e687ac48e232
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-15 22:30:05 -0600

    creating reducer function

---
+++ src/reducer.js
@@ -0,0 +1,31 @@
+let id = 0;
+function reducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return [
+        ...state,
+        {
+          id: ++id,
+          task: action.payload.task,
+          completed: false,
+        },
+      ];
+    case 'REMOVE_TASK':
+      return state.filter((task) => task.id !== action.payload.id);
+    default:
+      return state;
+  }
+  //   if (action.type === 'ADD_TASK') {
+  //     return [
+  //       ...state,
+  //       {
+  //         id: ++id,
+  //         task: action.payload.task,
+  //         completed: false,
+  //       },
+  //     ];
+  //   } else if (action.type === 'REMOVE_TASK') {
+  //     return state.filter((task) => task.id !== action.payload.id);
+  //   }
+  //   return state;
+}
