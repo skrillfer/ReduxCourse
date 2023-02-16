93d3934404109d91dbe03e409b14a4e243515780
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-16 08:35:55 -0600

    Calling an API using Redux Thunk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import { addTask, removeTask, updateTask } from './store/tasks';
+import { addTask, fetchTodo, removeTask, updateTask } from './store/tasks';
 import store from './store/configureStore';
 
 const unsubscribe = store.subscribe(() =>
   console.log('Updated', store.getState())
 );
 
-store.dispatch(addTask('Task1'));
+store.dispatch(addTask('TaskM'));
+store.dispatch(addTask('TaskP'));
 store.dispatch(updateTask({ id: 1, completed: true }));
 
-unsubscribe();
+// store.dispatch(removeTask(1));
+store.dispatch(fetchTodo());
+
+// unsubscribe();
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -19,6 +19,12 @@ export const removeTask = (id) => ({
   payload: { id },
 });
 
+export const fetchTodo = () => async (dispatch, getState) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const task = await response.json();
+  dispatch(addTask(task.title));
+};
+
 // Reducer
 let id = 0;
 export default function reducer(state = [], action) {
