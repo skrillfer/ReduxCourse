5ffa9a95cca2542246e7e8e7133c90f15d50a9ce
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-15 22:47:20 -0600

    creating actions, and dispatching to the store

---
+++ src/action.js
@@ -0,0 +1,3 @@
+export const addTask = (task) => ({ type: 'ADD_TASK', payload: { task } });
+
+export const removeTask = (id) => ({ type: 'REMOVE_TASK', payload: { id } });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { addTask, removeTask } from './action';
 import store from './store';
 
+store.dispatch(addTask('Task1'));
+store.dispatch(removeTask(1));
 console.log(store.getState());
