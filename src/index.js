9e093539e8551eff5d95c2137cde6c40582695e7
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-16 23:09:12 -0600

    creating employeeSlice, set to Store and use it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { addTask, fetchTodo, removeTask, updateTask } from './store/tasks';
 import store from './store/configureStore';
+import { addEmployee } from './store/employees';
 
 const unsubscribe = store.subscribe(() =>
   console.log('Updated', store.getState())
@@ -8,7 +9,7 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(addTask({ task: 'Task 1' }));
 store.dispatch(addTask({ task: 'Task 2' }));
 store.dispatch(updateTask({ task: { id: 1, completed: true } }));
-
+store.dispatch(addEmployee({ employee: 'Ramirez' }));
 // store.dispatch(removeTask(1));
 store.dispatch(fetchTodo());
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,8 @@
-import reducer from './tasks';
+import taskReducer from './tasks';
+import employeeReducer from './employees';
+
 import { configureStore } from '@reduxjs/toolkit';
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: { taskReducer, employeeReducer } });
 
 export default store;
