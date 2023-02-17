2f481d9fb15f629c9e86ab7b62a798adf8b505c3
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-16 22:51:22 -0600

    createSlice with Redux Toolkit

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,57 +1,35 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+let id = 0;
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    // action: function
+    addTask: (state, action) => {
+      state.push({
+        id: ++id,
+        task: action.payload.task,
+        completed: false,
+      });
+    },
+    updateTask: (state, action) => {
+      const index = state.findIndex(
+        (task) => task.id === action.payload.task.id
+      );
+      state[index] = { ...state[index], ...action.payload.task };
+    },
+    removeTask: (state, action) => {
+      const index = state.findIndex((task) => task.id === action.payload.id);
+      state.splice(index, 1);
+    },
+  },
+});
 
+export const { addTask, removeTask, updateTask } = taskSlice.actions;
+export default taskSlice.reducer;
 // Actions
-export const addTask = createAction('ADD_TASK');
-export const updateTask = createAction('UPDATE_TASK');
-export const removeTask = createAction('REMOVE_TASK');
-
 export const fetchTodo = () => async (dispatch, getState) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
   const task = await response.json();
   dispatch(addTask({ task: task.title }));
 };
-
-// Reducer
-let id = 0;
-
-createReducer([], {
-  [addTask.type]: (state, action) => {
-    state.push({
-      id: ++id,
-      task: action.payload.task,
-      completed: false,
-    });
-  },
-  [updateTask.type]: (state, action) => {
-    const index = state.findIndex((task) => task.id === action.payload.id);
-    state[index] = { ...taskToUpdate, ...task };
-  },
-  [removeTask.type]: (state, action) => {
-    const index = state.findIndex((task) => task.id === action.payload.id);
-    state.splice(index, 1);
-  },
-});
-
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case addTask.type:
-      return [
-        ...state,
-        {
-          id: ++id,
-          task: action.payload.task,
-          completed: false,
-        },
-      ];
-    case updateTask.type:
-      return state.map((task) =>
-        task.id === action.payload.task.id
-          ? { ...task, ...action.payload.task }
-          : task
-      );
-    case removeTask.type:
-      return state.filter((task) => task.id !== action.payload.id);
-    default:
-      return state;
-  }
-}
