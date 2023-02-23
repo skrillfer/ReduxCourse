7b9544799eb12548de6b251d99041665c2da3139
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-22 22:14:27 -0600

    fetching using createAsyncThunk and handling error, using respective states

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,29 +1,62 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 let id = 0;
+
+const initialState = {
+  tasks: [],
+  loading: false,
+  error: null,
+};
+export const fetchTasks = createAsyncThunk(
+  'fetchTasks',
+  async (a, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('http://localhost:5500/api/tasks');
+      return { tasks: response.data };
+    } catch (error) {
+      return rejectWithValue({ error: error.message });
+    }
+  }
+);
+
 const taskSlice = createSlice({
   name: 'tasks',
-  initialState: [],
+  initialState,
   reducers: {
-    getTasks: (state, action) => {
-      return action.payload.tasks;
-    },
     // action: function
+    getTasks: (state, action) => {
+      state.tasks = action.payload.tasks;
+    },
     addTask: (state, action) => {
-      state.push({
+      state.tasks.push({
         id: ++id,
         task: action.payload.task,
         completed: false,
       });
     },
     updateTask: (state, action) => {
-      const index = state.findIndex(
+      const index = state.tasks.findIndex(
         (task) => task.id === action.payload.task.id
       );
-      state[index] = { ...state[index], ...action.payload.task };
+      state.tasks[index] = { ...state.tasks[index], ...action.payload.task };
     },
     removeTask: (state, action) => {
-      const index = state.findIndex((task) => task.id === action.payload.id);
-      state.splice(index, 1);
+      const index = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      state.tasks.splice(index, 1);
+    },
+  },
+  extraReducers: {
+    [fetchTasks.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchTasks.fulfilled]: (state, action) => {
+      state.tasks = action.payload.tasks;
+      state.loading = false;
+    },
+    [fetchTasks.rejected]: (state, action) => {
+      state.error = action.payload.error;
     },
   },
 });
