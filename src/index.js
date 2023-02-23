b0ef76e0a0d92cd5d2391e06d59a71faa0af56bf
Luis Fernando Ramírez Santos <rluis4490@gmail.com>
2023-02-22 22:14:27 -0600

    Making API Request using a simple function and handling error using redux also

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import store from './store/configureStore';
-import { getTasks } from './store/tasks';
+import { fetchTasks } from './store/tasks';
 
-const gettingTasks = async () => {
-  // calling api
-  try {
-    const response = await axios.get('http://localhost:5500/api/tasks');
-    console.log(response);
-    store.dispatch(getTasks({ tasks: response.data }));
-  } catch (error) {
-    store.dispatch({ type: 'SHOW_ERROR', payload: { error: error.message } });
-  }
-};
-gettingTasks();
+store.dispatch(fetchTasks());
+// const gettingTasks = async () => {
+//   // calling api
+//   try {
+//     const response = await axios.get('http://localhost:5500/api/tasks');
+//     console.log(response);
+//     store.dispatch(getTasks({ tasks: response.data }));
+//   } catch (error) {
+//     store.dispatch({ type: 'SHOW_ERROR', payload: { error: error.message } });
+//   }
+// };
+// gettingTasks();
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,30 +1,65 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from '../utils/http';
 let id = 0;
+
+const initialState = {
+  tasks: [],
+  loading: false,
+  error: null,
+};
+export const fetchTasks = createAsyncThunk(
+  'fetchTasks',
+  async (a, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/tasks');
+      return { tasks: response.data };
+    } catch (error) {
+      return rejectWithValue({ error: error.message });
+    }
+  }
+);
+
 const taskSlice = createSlice({
   name: 'tasks',
-  initialState: [],
+  initialState,
   reducers: {
-    getTasks: (state, action) => {
-      return action.payload.tasks;
-    },
     // action: function
+    getTasks: (state, action) => {
+      state.tasks = action.payload.tasks;
+    },
     addTask: (state, action) => {
-      state.push({
+      state.tasks.push({
         id: ++id,
         task: action.payload.task,
         completed: false,
       });
     },
     updateTask: (state, action) => {
-      const index = state.findIndex(
+      const index = state.tasks.findIndex(
         (task) => task.id === action.payload.task.id
       );
-      state[index] = { ...state[index], ...action.payload.task };
+      state.tasks[index] = { ...state.tasks[index], ...action.payload.task };
     },
     removeTask: (state, action) => {
-      const index = state.findIndex((task) => task.id === action.payload.id);
-      state.splice(index, 1);
+      const index = state.tasks.findIndex(
+        (task) => task.id === action.payload.id
+      );
+      state.tasks.splice(index, 1);
     },
+  },
+  extraReducers: (builder) => {
+    const { pending, fulfilled, rejected } = fetchTasks;
+    builder
+      .addCase(pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(fulfilled, (state, action) => {
+        state.tasks = action.payload.tasks;
+        state.loading = false;
+      })
+      .addCase(rejected, (state, action) => {
+        state.error = action.payload.error;
+      });
   },
 });
 
